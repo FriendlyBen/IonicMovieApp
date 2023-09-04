ca11c6445d7bed5f5f4566321700b37a805bde3d
Bentley Teh <bentley.teh@amg.com>
2023-09-04 16:58:22 +0800

    Fix image bug for android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'e777a70fcfd016e176e0649c1daaab4e',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'https://image.tmdb.org/t/p'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiKey: 'e777a70fcfd016e176e0649c1daaab4e',
   baseUrl: 'https://api.themoviedb.org/3',
-  images: 'http://image.tmdb.org/t/p'
+  images: 'https://image.tmdb.org/t/p'
 };
 
 /*
